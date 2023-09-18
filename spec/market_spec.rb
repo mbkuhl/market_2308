f8c06a55747f20163df5527ff1d8b79d6896a33d
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-09-18 11:11:45 -0600

    Write tests for #sell

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -110,4 +110,62 @@ RSpec.describe Market do
       expect(@market.sorted_item_list).to eq(["Banana Nice Cream", "Chocolate Nice Cream", 'Peach', "Peach-Raspberry Nice Cream", 'Tomato'])
     end
   end
+
+  describe 'item_object list' do
+    it 'can return a list of all item object sold at the market in no particular order' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.item_object_list.include?(@item1)).to be true
+      expect(@market.item_object_list.include?(@item2)).to be true
+      expect(@market.item_object_list.include?(@item3)).to be true
+      expect(@market.item_object_list.include?(@item4)).to be true
+    end
+  end
+
+  describe '#total_item_stock' do
+    it 'can return the total stock of an item at the market' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.total_item_stock(@item1)).to eq(100)
+      expect(@market.total_item_stock(@item2)).to eq(7)
+      expect(@market.total_item_stock(@item3)).to eq(25)
+      expect(@market.total_item_stock(@item4)).to eq(50)
+      @vendor2.stock(@item4, 1)
+      expect(@market.total_item_stock(@item4)).to eq(51)
+    end
+  end
+
+  describe '#date' do
+
+  end
+
+  describe '#sell' do
+    it 'will not sell items if there is not enough total inventory' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.sell(@item2, 8)).to be false
+      expect(@market.total_item_stock(@item2)).to eq(7)
+    end
+
+    it 'will sell items from the first vendor added to the market' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.sell(@item3, 15)).to be true
+      expect(@market.total_item_stock(@item3)).to eq(10)
+    end
+
+    it 'will sell items from multiple vendors if one vendor does not have enough stock (starting with the first vendor added to the market)' do
+      @market.add_vendor(@vendor1)
+      @market.add_vendor(@vendor2)
+      @market.add_vendor(@vendor3)
+      expect(@market.sell(@item1, 50)).to be true
+      expect(@market.total_item_stock(@item1)).to eq(50)
+      expect(@vendor1.check_stock(@item1)).to eq(0)
+      expect(@vendor3.check_stock(@item1)).to eq(50)
+    end
+  end
 end
